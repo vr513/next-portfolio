23be926a4d3489e4c3b6367607ba393f7982cbba
Varad Rajopadhye <varadmanraj@gmail.com>
2022-10-07 19:40:44 +0530

    Test Analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,17 +1,22 @@
-import React from 'react'
-import { ChakraProvider, ColorModeProvider, useColorMode } from '@chakra-ui/react'
-import customTheme from '../styles/theme'
-import { Global, css } from '@emotion/react'
+import React from "react";
+import {
+  ChakraProvider,
+  ColorModeProvider,
+  useColorMode,
+} from "@chakra-ui/react";
+import customTheme from "../styles/theme";
+import { Global, css } from "@emotion/react";
+import Script from "next/script";
 
 const GlobalStyle = ({ children }) => {
-  const { colorMode } = useColorMode()
+  const { colorMode } = useColorMode();
 
   return (
     <>
       <Global
         styles={css`
           ::selection {
-            background-color: #90CDF4;
+            background-color: #90cdf4;
             color: #fefefe;
           }
           ::-moz-selection {
@@ -26,17 +31,17 @@ const GlobalStyle = ({ children }) => {
             display: flex;
             flex-direction: column;
             min-height: 100vh;
-            background: ${colorMode === 'light' ? 'white' : '#171717'};
+            background: ${colorMode === "light" ? "white" : "#171717"};
           }
           ::-webkit-scrollbar {
             width: 0.8em;
           }
-          
+
           ::-webkit-scrollbar-thumb {
-            background: ${colorMode === 'gray' ? '#171717' : 'gray'};
+            background: ${colorMode === "gray" ? "#171717" : "gray"};
             border-radius: 100vw;
           }
-          
+
           // ::-webkit-scrollbar-thumb:hover {
           //   background: rgba(31, 30, 30, 0.726);
           // }
@@ -44,24 +49,41 @@ const GlobalStyle = ({ children }) => {
       />
       {children}
     </>
-  )
-}
+  );
+};
 
 function MyApp({ Component, pageProps }) {
   return (
-    <ChakraProvider resetCSS theme={customTheme}>
-      <ColorModeProvider
-        options={{
-          initialColorMode: "light",
-          useSystemColorMode: true,
-        }}
-      >
-        <GlobalStyle>
-          <Component {...pageProps} />
-        </GlobalStyle>
-      </ColorModeProvider>
-    </ChakraProvider>
-  )
+    <>
+      <Script
+        strategy="lazyOnload"
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+      />
+
+      <Script strategy="lazyOnload">
+        {`
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+        page_path: window.location.pathname,
+        });
+    `}
+      </Script>
+      <ChakraProvider resetCSS theme={customTheme}>
+        <ColorModeProvider
+          options={{
+            initialColorMode: "light",
+            useSystemColorMode: true,
+          }}
+        >
+          <GlobalStyle>
+            <Component {...pageProps} />
+          </GlobalStyle>
+        </ColorModeProvider>
+      </ChakraProvider>
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
