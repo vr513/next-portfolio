49c7178863d979162b65b20c75e4c7e6e1309e3b
Varad Rajopadhye <varadmanraj@gmail.com>
2022-11-06 10:35:53 +0530

    CORS ENABLE

--- next.config.js
+++ next.config.js
@@ -11,13 +11,19 @@ module.exports = {
   eslint: {
     ignoreDuringBuilds: true,
   },
-  async rewrites() {
-        return [
-          {
-            source: '/api/:path*',
-            destination: 'https://varadrajopadhye.tech/api/:path*',
-          },
+  async headers() {
+    return [
+      {
+        // matching all API routes
+        source: "/api/:path*",
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          { key: "Access-Control-Allow-Origin", value: "*" },
+          { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
+          { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
         ]
-      },
+      }
+    ]
+  },
   "plugins": [["styled-components", { "ssr": true }]]
 }
