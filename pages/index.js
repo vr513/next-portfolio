9a2a4bfb3db042e60f02e3f3c9ad1429487c9aa0
Varad Rajopadhye <varadmanraj@gmail.com>
2023-08-13 09:25:16 +0530

    Convert to pnpm added typescript support

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 import Head from "next/head";
-import Home from "../components/Home";
+import Home from "../components/Home.jsx";
 import NavBar from "../components/NavBar";
 import Footer from "../components/Footer";
 
