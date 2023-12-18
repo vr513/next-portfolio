9874c39949af4b6f9b89d8c7c96cc837b670c4bb
Varad Rajopadhye <varadmanraj@gmail.com>
2023-12-18 20:01:35 +0530

    Update caching strategy

--- pages/index.js
+++ pages/index.js
@@ -99,6 +99,7 @@ export async function getServerSideProps() {
   const docs = await getDocLinks();
   return {
     props: { docs }, // will be passed to the page component as props
-    revalidate: 21600,
   };
 }
+
+export const unstable_revalidate = 21600; // In seconds. Adjust as needed based on your caching strategy.
